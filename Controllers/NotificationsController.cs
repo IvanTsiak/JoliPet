d9616292fdcf05611fede19b4c93cccc6e07c07d
IvanTsiak <ivanzjak2007@gmail.com>
2026-05-10 00:04:18 +0300

    impement NotificationsController's methods

--- Controllers/NotificationsController.cs
+++ Controllers/NotificationsController.cs
@@ -1,6 +1,52 @@
-﻿namespace JoliPet.Controllers;
+﻿using JoliPet.DTOs;
+using JoliPet.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-public class NotificationsController
+namespace JoliPet.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NotificationsController : ControllerBase
 {
+    private readonly JoliPetContext _context;
     
+    public NotificationsController(JoliPetContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<NotificationDto>>> GetNotifications()
+    {
+        var unreadNotifications = await _context.Notifications
+            .Include(n => n.User)
+            .Where(n => n.IsRead == false)
+            .Select(n => new NotificationDto
+            {
+                Id = n.Id,
+                UserName = n.User.Username,
+                Message = n.Message,
+                CreatedAt = n.CreatedAt,
+            })
+            .ToListAsync();
+
+        return Ok(unreadNotifications);
+    }
+
+    [HttpPost("{id}/read")]
+    public async Task<IActionResult> ReadNotification([FromRoute] int id)
+    {
+        var notification = await _context.Notifications.FindAsync(id);
+
+        if (notification == null)
+        {
+            return NotFound(new { message = "Not found" });
+        }
+
+        notification.IsRead = true;
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
 }
---
+++ DTOs/NotificationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace JoliPet.DTOs;
+
+public class NotificationDto
+{
+    public int Id { get; set; }
+    public required string UserName { get; set; }
+    public required string Message { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
